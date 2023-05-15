32a59cc1883997f6b16e3feb2715ba92a64f6bbd
Mo Mahboobian <momahboobian@gmail.com>
2023-05-15 23:05:57 +0100

    AddToCard component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Header } from "../../components/Header";
+import AddToCard from "../../components/AddToCard";
 
 export const Home = () => {
   return (
     <div>
       <Header />
+      <AddToCard />
     </div>
   );
 };
