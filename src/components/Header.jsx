aba3dc6a08945b1a9e6afe4331cd08b9da722a3c
Lorena <lorenacapraru@gmail.com>
2023-05-15 21:01:57 +0100

    menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,6 +9,13 @@ export const Header = () => {
         <div className="headerLeft">
           <Sidebar />
           <h1>sneakers</h1>
+            <ul>
+              <li>Collections</li>
+              <li>Men</li>
+              <li>Women</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
         </div>
 
         <div className="headerRight">
