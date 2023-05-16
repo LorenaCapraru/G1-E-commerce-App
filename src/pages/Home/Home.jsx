f8803d64d9e4c464131d8383311cf62ed2b2b312
Lorena <lorenacapraru@gmail.com>
2023-05-16 10:23:31 +0100

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { Header } from "../../components/Header";
-<<<<<<< Updated upstream
-=======
 import {AddToCard} from "../../components/AddToCard";
->>>>>>> Stashed changes
+
 import "./Home.css";
 
 export const Home = () => {
