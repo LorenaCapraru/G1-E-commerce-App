d550b1d29ecabfdc5d03a1d9e5f63fe1af8c21f6
nataliizab <nataliia.zab@gmail.com>
2023-06-07 21:33:14 +0100

    Added Routes, deleted Men section from Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 // App.js
 import "./App.css";
 import { Home } from "./pages/Home/Home";
+import  About  from "./pages/About/About";
+// import {Contact } from "./pages/Contact/Contact";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      <Home />
-      
-    </div>
+   
+     <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
+    </Router>
   );
 };
 
